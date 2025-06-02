e30ed48b9dd9ccb392c50cf465355fc61d6b5764
Prabhavathi-Godatha <prabhavathigodatha15@gmail.com>
2025-06-02 11:57:22 +0530

    incoming changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
 import "../src/app.css";
 import PageRoutes from "./componets/PageRoutes";
-<<<<<<< HEAD
-
-=======
->>>>>>> a604835a413b68627c55e95b6c344a186b59c786
 
 function App() {
   return (
     <>
       <PageRoutes />
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-      {/* <Loginpage /> */}
-=======
->>>>>>> 90503ee019493561a2e377c8c376ca3e26dd6065
->>>>>>> a604835a413b68627c55e95b6c344a186b59c786
     </>
   );
 }
--- src/componets/PageRoutes.jsx
+++ src/componets/PageRoutes.jsx
@@ -2,11 +2,6 @@ import { Route, Routes } from "react-router-dom";
 import { lazy, Suspense } from "react";
 
 const PageRoutes = () => {
-<<<<<<< HEAD
-  const Loginpage = lazy(() => import("../componets/LoginPage"))
-  const SubCategories = lazy(() => import("../componets/Pages/SubCategories"))
-  const Profile = lazy(() => import("./Profile"))
-=======
   const ProductDetails = lazy(() =>
     import("../componets/Pages/ProductDetails.jsx")
   );
@@ -22,7 +17,7 @@ const PageRoutes = () => {
   const OrderDetails = lazy(() => import("./MyorderStatus.jsx"));
   const Header = lazy(() => import("../componets/common/Header.jsx"));
   const StatusPage = lazy(() => import("./Pages/StatusPage.jsx"));
->>>>>>> a604835a413b68627c55e95b6c344a186b59c786
+
   return (
     <>
       <Suspense>
@@ -31,20 +26,14 @@ const PageRoutes = () => {
           <Route path="/Header" element={"<Header/>"} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/subCategories" element={<SubCategories />} />
-<<<<<<< HEAD
-          <Route path="/profile" element={<Profile />} />
-=======
+          {/* <Route path="/profile" element={<Profile />} /> */}
           <Route path="/ProductDetails" element={<ProductDetails />} />
           <Route path="/loginpage" element={<Loginpage />} />
-<<<<<<< HEAD
-
-=======
           <Route path="/subCategories" element={<SubCategories />} />
           {/* <Route path="/OrderDetails" element={<OrderDetails />} /> */}
           <Route path="/StatusPage" element={<StatusPage />} />
           <Route path="/AllOrders" element={<AllOrders />} />
->>>>>>> 90503ee019493561a2e377c8c376ca3e26dd6065
->>>>>>> a604835a413b68627c55e95b6c344a186b59c786
+
         </Routes>
       </Suspense>
     </>
